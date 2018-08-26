d4f4e627a6d664951e7bfcd131a8893bb02fca4c
markoYam <marco.yam.catina@gmail.com>
2018-08-26 17:57:16 +0200

    Agregar proyecto a repositorio

---
+++ Librerias/dialog/src/main/java/com/adminyam/dialog/Activity/SimpleDialogActivity.java
@@ -0,0 +1,15 @@
+package com.adminyam.dialog.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.adminyam.dialog.R;
+
+public class SimpleDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_dialog);
+    }
+}
